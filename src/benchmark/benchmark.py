285069633786390f7be07e08e96e9c41c9ad5c6f
Patrick O'Neill <pon2@umbc.edu>
2014-07-31 13:57:29 -0400

    benchmark

---
+++ src/benchmark/benchmark.py
@@ -0,0 +1,12 @@
+"""
+
+The purpose of this script is:
+
+    (1) to determine a common interface for TF-DNA lattice models, and
+    (2) to provide benchmarks for different sampling algorithms.
+
+The fundamental inputs to the model are:
+    (1) A DNA sequence, which may be either circular or linear
+    (2) A Hamiltonian that indirectly determines the probability of the system occupying any particular configuration.
+
+"""
